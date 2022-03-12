15879decd20922ed7adc82df55635a61730d77dd
graygray07 <graymgray07@gmail.com>
2022-03-11 17:27:07 -0700

    Add Sample PTPOV ( next year )

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SAMPLEptpov.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+
+import java.util.List;
+
+
+@TeleOp(name="Pushbot: Teleop POV", group="Pushbot")
+@Disabled
+public class SAMPLEptPOV extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    HardwarePushbot robot           = new HardwarePushbot();   // Use a Pushbot's hardware
+
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double drive;
+        double turn;
+        double max;
+        double slowMode = 0; //0 is off
+        // DcMotor motorFrontLeft;
+        //DcMotor motorBackLeft;
+        //DcMotor motorFrontRight;
+        //DcMotor motorBackRight;
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+            if (gamepad1.a && slowMode==0){
+                slowMode=1;
+            }else if (gamepad1.a && slowMode==1){
+                slowMode=0;
+            }
+            if (slowMode==1){
+                backRightPower /=2;
+                backLeftPower /=2;
+                frontRightPower /=2;
+                frontLeftPower /=2;
+            }else{
+                backRightPower /=1;
+                backLeftPower /=1;
+                frontRightPower /=1;
+                frontLeftPower /=1;
+            }
+
+            telemetry.update();
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+}
